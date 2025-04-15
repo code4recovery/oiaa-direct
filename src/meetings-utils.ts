40a5678622f81c4d053e7c26eb4b9fed4af4c5d0
Tim Rohrer <tim@tgr66.dev>
2025-04-15 07:17:32 -0700

    refactor: Add factory to make getMeetings more generic; clean up duplicate.

--- src/meetings-utils.ts
+++ src/meetings-utils.ts
@@ -1,12 +1,6 @@
 import { matchSorter } from "match-sorter"
 
-import type {
-  Community,
-  Feature,
-  Format,
-  Meeting,
-  Type,
-} from "./meetingTypes"
+import type { Community, Feature, Format, Meeting, Type } from "./meetingTypes"
 
 // TODO This can extend CategoryMap?
 export interface FilterParams {
@@ -65,45 +59,6 @@ export const fetchMeetings = async (url: string): Promise<Meeting[]> => {
     console.error(error)
     return []
   }
-}
-
-export const getMeetings = async (
-  filter?: FilterParams
-): Promise<Meeting[]> => {
-  if (!import.meta.env.VITE_CQ_URL)
-    throw new Error("App not configured correctly")
-  const url = import.meta.env.VITE_CQ_URL as string
-  let meetings = await fetchMeetings(url)
-
-  if (filter) {
-    const { nameQuery, features, formats, type, communities, languages } =
-      filter
-    const applyFilters = [
-      type
-        ? (data: Meeting[]) => filteredData(data, [type], "type")
-        : undefined,
-      formats
-        ? (data: Meeting[]) => filteredData(data, formats, "formats")
-        : undefined,
-      features
-        ? (data: Meeting[]) => filteredData(data, features, "features")
-        : undefined,
-      communities
-        ? (data: Meeting[]) => filteredData(data, communities, "communities")
-        : undefined,
-      languages
-        ? (data: Meeting[]) => filteredData(data, languages, "languages")
-        : undefined,
-      nameQuery
-        ? (data: Meeting[]) =>
-            fuzzyGlobalTextFilter(data, ["name", "slug"], nameQuery)
-        : undefined,
-    ].filter(Boolean) as ((data: Meeting[]) => Meeting[])[]
-
-    meetings = applyFilters.reduce((data, filterFn) => filterFn(data), meetings)
-  }
-
-  return meetings
 }
 
 export const buildFilter = (
