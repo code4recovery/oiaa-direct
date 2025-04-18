ebbbdbaf04c6f69a894c84f2563b2df58c4afa3a
Tim Rohrer <tim@tgr66.dev>
2025-04-18 10:48:13 -0700

    refactor: Move applyMeetingFilters to meeting-utils and move filter to route data loader.

--- src/getMeetings.ts
+++ src/getMeetings.ts
@@ -1,9 +1,4 @@
-import type { FilterParams } from "./meetings-utils"
-import {
-  fetchData,
-  filteredData,
-  fuzzyGlobalTextFilter,
-} from "./meetings-utils"
+import { fetchData } from "./meetings-utils"
 import type { Meeting } from "./meetingTypes"
 
 /**
@@ -12,46 +7,13 @@ import type { Meeting } from "./meetingTypes"
  * @param filter - Optional filter parameters to apply to the fetched meetings.
  * @returns A promise that resolves to an array of filtered meetings.
  */
-export const createMeetingFetcher =
-  (urlAddendum: string) =>
-  async (filter?: FilterParams): Promise<Meeting[]> => {
-    if (!import.meta.env.VITE_CQ_URL)
-      throw new Error("App not configured correctly")
-    const url = import.meta.env.VITE_CQ_URL as string
-    let meetings = await fetchData<Meeting>(`${url}/${urlAddendum}`)
+export const createMeetingFetcher = async (urlAddendum: string) => {
+  if (!import.meta.env.VITE_CQ_URL)
+    throw new Error("App not configured correctly")
+  const url = import.meta.env.VITE_CQ_URL as string
+  const meetings = await fetchData<Meeting>(`${url}/${urlAddendum}`)
 
-    if (filter) {
-      const { nameQuery, features, formats, type, communities, languages } =
-        filter
-      const applyFilters = [
-        type
-          ? (data: Meeting[]) => filteredData(data, [type], "type")
-          : undefined,
-        formats
-          ? (data: Meeting[]) => filteredData(data, formats, "formats")
-          : undefined,
-        features
-          ? (data: Meeting[]) => filteredData(data, features, "features")
-          : undefined,
-        communities
-          ? (data: Meeting[]) => filteredData(data, communities, "communities")
-          : undefined,
-        languages
-          ? (data: Meeting[]) => filteredData(data, languages, "languages")
-          : undefined,
-        nameQuery
-          ? (data: Meeting[]) =>
-              fuzzyGlobalTextFilter(data, ["name", "slug"], nameQuery)
-          : undefined,
-      ].filter(Boolean) as ((data: Meeting[]) => Meeting[])[]
+  return meetings
+}
 
-      meetings = applyFilters.reduce(
-        (data, filterFn) => filterFn(data),
-        meetings
-      )
-    }
-
-    return meetings
-  }
-
-export const getNextMeetings = createMeetingFetcher("next")
+export const getNextMeetings = () => createMeetingFetcher("next")
--- src/meetings-utils.ts
+++ src/meetings-utils.ts
@@ -1,6 +1,6 @@
 import { matchSorter } from "match-sorter"
 
-import type { Community, Feature, Format, Type } from "./meetingTypes"
+import type { Community, Feature, Format, Meeting, Type } from "./meetingTypes"
 
 // TODO This can extend CategoryMap?
 export interface FilterParams {
@@ -75,4 +75,38 @@ export const buildFilter = (
 export const toggleArrayElement = <T>(array: T[], value: T): T[] => {
   const newArray = array.filter((x) => x !== value)
   return newArray.length === array.length ? [...newArray, value] : newArray
+}
+
+/**
+ * Applies filters to a list of meetings based on the provided filter parameters.
+ * @param meetings - The list of meetings to filter.
+ * @param filter - The filter parameters to apply.
+ * @returns The filtered list of meetings.
+ */
+export const applyMeetingFilters = (
+  meetings: Meeting[],
+  filter: FilterParams
+): Meeting[] => {
+  const { nameQuery, features, formats, type, communities, languages } = filter
+  const applyFilters = [
+    type ? (data: Meeting[]) => filteredData(data, [type], "type") : undefined,
+    formats
+      ? (data: Meeting[]) => filteredData(data, formats, "formats")
+      : undefined,
+    features
+      ? (data: Meeting[]) => filteredData(data, features, "features")
+      : undefined,
+    communities
+      ? (data: Meeting[]) => filteredData(data, communities, "communities")
+      : undefined,
+    languages
+      ? (data: Meeting[]) => filteredData(data, languages, "languages")
+      : undefined,
+    nameQuery
+      ? (data: Meeting[]) =>
+          fuzzyGlobalTextFilter(data, ["name", "slug"], nameQuery)
+      : undefined,
+  ].filter(Boolean) as ((data: Meeting[]) => Meeting[])[]
+
+  return applyFilters.reduce((data, filterFn) => filterFn(data), meetings)
 }
