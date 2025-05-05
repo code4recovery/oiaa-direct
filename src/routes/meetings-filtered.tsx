f16dcddaf017fa886b1181cb3e6dfef2648cf2f7
Tim Rohrer <tim@tgr66.dev>
2025-05-05 07:39:22 -0700

    feat: Add basic expanded search options when no results are available.

--- src/routes/meetings-filtered.tsx
+++ src/routes/meetings-filtered.tsx
@@ -4,20 +4,23 @@ import {
 } from "react-router"
 
 import { Filter } from "@/components/Filter"
+import FullSearch from "@/components/FullSearch"
 import { Layout } from "@/components/Layout"
 import { MeetingsSummary } from "@/components/MeetingsSummary"
 import { getMeetings } from "@/getData"
 import {
   applyMeetingFilters,
   buildFilter,
-} from "@/meetings-utils"
+} from "@/utils/meetings-utils"
+import { Text } from "@chakra-ui/react"
 
 import type { Route } from "./+types/meetings-filtered"
 
 export async function clientLoader({ request }: Route.ClientLoaderArgs) {
   const { searchParams } = new URL(request.url)
+  const qs = `?${searchParams.toString()}`
   const filter = buildFilter(searchParams)
-  const unfilteredMeetings = await getMeetings()
+  const unfilteredMeetings = await getMeetings(qs)
 
   const meetings = applyMeetingFilters(unfilteredMeetings, filter)
   console.log(meetings)
@@ -27,6 +30,7 @@ export async function clientLoader({ request }: Route.ClientLoaderArgs) {
 
 export default function MeetingsFiltered({ loaderData }: Route.ComponentProps) {
   const [filterParams, setFilterParams] = useSearchParams()
+  const { meetings } = loaderData
 
   const handleQuery = (query: string) => {
     setFilterParams((prev) => {
@@ -39,14 +43,28 @@ export default function MeetingsFiltered({ loaderData }: Route.ComponentProps) {
     <>
       <Layout
         sidebar={
-          <Filter
-            filterParams={filterParams}
-            sendFilterSelectionsToParent={setFilterParams}
-            sendQueryToParent={handleQuery}
-          />
+          <>
+            {meetings.length === 0 && (
+              <FullSearch
+                filterParams={filterParams}
+                sendFilterSelectionsToParent={setFilterParams}
+              />
+            )}
+            <Filter
+              filterParams={filterParams}
+              sendFilterSelectionsToParent={setFilterParams}
+              sendQueryToParent={handleQuery}
+            />
+          </>
         }
       >
-        <MeetingsSummary meetings={loaderData.meetings} />
+        {meetings.length > 0 ? (
+          <MeetingsSummary meetings={meetings} />
+        ) : (
+          <Text textAlign="center" py={8} color="gray.500">
+            No meetings found matching your criteria.
+          </Text>
+        )}
       </Layout>
       <Outlet />
     </>
