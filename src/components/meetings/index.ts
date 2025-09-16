b2e426b0d360788553e81c65fad847dde5c499ba
Tim Rohrer <tim@tgr66.dev>
2025-09-16 10:04:48 -0700

    chore: Remove outdated comments from meeting components index.

--- src/components/meetings/index.ts
+++ src/components/meetings/index.ts
@@ -1,26 +1,23 @@
 // Meeting Components - Barrel Exports
 // Atomic components for building meeting displays
 
-export { default as MeetingTime } from './MeetingTime'
-export type { MeetingTimeProps } from './MeetingTime'
+export { default as MeetingTime } from "./MeetingTime"
+export type { MeetingTimeProps } from "./MeetingTime"
 
-export { default as MeetingCategories } from './MeetingCategories'
-export type { MeetingCategoriesProps } from './MeetingCategories'
+export { default as MeetingCategories } from "./MeetingCategories"
+export type { MeetingCategoriesProps } from "./MeetingCategories"
 
-export { default as QuickActions } from './QuickActions'
-export type { QuickActionsProps } from './QuickActions'
+export { default as QuickActions } from "./QuickActions"
+export type { QuickActionsProps } from "./QuickActions"
 
-export { default as CalendarActions } from './CalendarActions'
-export type { CalendarActionsProps } from './CalendarActions'
+export { default as CalendarActions } from "./CalendarActions"
+export type { CalendarActionsProps } from "./CalendarActions"
 
-// Composite components
-export { default as MeetingItem } from './MeetingItem'
-export type { MeetingItemProps } from './MeetingItem'
+export { default as MeetingItem } from "./MeetingItem"
+export type { MeetingItemProps } from "./MeetingItem"
 
-export { MeetingsSummary } from './MeetingsSummary'
+export { MeetingsSummary } from "./MeetingsSummary"
 
-// Action components
-export { default as JoinMeetingButton } from './JoinMeetingButton'
+export { default as JoinMeetingButton } from "./JoinMeetingButton"
 
-// Legacy components (consider deprecating)
-export { MeetingCard } from './MeetingCard'
+export { MeetingCard } from "./MeetingCard"
