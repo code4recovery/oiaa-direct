b4c1cebbe85069325e9d1485c59ba9aa9a3bcb0e
JDV Main Laptop <websup@suffolkny-aa.org>
2025-12-23 12:06:49 -0500

    another merge error

--- src/utils/meetings-utils.test.ts
+++ src/utils/meetings-utils.test.ts
@@ -19,10 +19,6 @@ describe("toggleArrayElement", () => {
     const testArray: string[] = ["A"]
     expect(toggleArrayElement(testArray, "A")).toEqual([])
   })
-  test("Second selection of A should return an empty array", () => {
-    const testArray: string[] = ["A"]
-    expect(toggleArrayElement(testArray, "A")).toEqual([])
-  })
 
   test("Second selection of B should return just A", () => {
     expect(toggleArrayElement(["A", "B"], "B")).toEqual(["A"])
