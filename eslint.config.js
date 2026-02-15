bdf69a19ac1eecbb6b45e6b06aa8417ba95f48cb
Steven Black <genseb7@gmail.com>
2026-02-15 15:56:46 -0500

    Add CI lint workflow and eslint-plugin-simple-import-sort
    - Add GitHub Actions workflow running ESLint and Prettier on PRs/pushes to main
    - Remove .github from .gitignore to allow workflow files
    - Add eslint-plugin-simple-import-sort to enforce import ordering

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -20,9 +21,12 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
